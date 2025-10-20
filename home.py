c525b6d959e408dc9a018477ee77ef1ab2d25f5f
bdarmstrong <bendyarmstrong@gmail.com>
2025-10-20 16:35:42 -0500

    Commented out all page code

--- home.py
+++ home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from sidebar import init_sidebar
 
-st.set_page_config(page_title="Lobbyist Data App", layout="wide")
+st.set_page_config(page_title="UChicago Building Energy & Emissions App", layout="wide")
 
-st.title("Welcome to the Lobbyist Data App!")
+st.title("Welcome to our energy & emissions tracker for UChicago's buildings!!")
 st.write("Use the sidebar to navigate to different pages.")
 
 init_sidebar()
--- sidebar.py
+++ sidebar.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 def init_sidebar():
-    st.sidebar.page_link('pages/Lobbyist_Search.py', label='Lobbyist Search')
-    st.sidebar.page_link('pages/Department_Search.py', label='Department Search')
-    st.sidebar.page_link('pages/Top_10_Charts.py', label='Top 10 Charts')
-    st.sidebar.page_link('pages/video.py', label='Video of progression')
+    st.sidebar.page_link('pages/building_map.py', label='Building Map')
+    st.sidebar.page_link('pages/building_search.py', label='Building Search')
+    st.sidebar.page_link('pages/building_charts.py', label='Misc Charts')
